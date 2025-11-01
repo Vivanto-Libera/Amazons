17dcc35e1c6604e49b04b14fdd1c3ace39726543
Vivanto <1425078256@qq.com>
2025-11-01 12:57:40 +0800

    now can trans index to move and move to index

--- Amazons_CPP/Board.h
+++ Amazons_CPP/Board.h
@@ -1,12 +1,16 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include <pybind11/pybind11.h>
+#include <map>
 
 namespace py = pybind11;
+using namespace pybind11::literals;
 
 class Board
 {
 public:
+	static const std::map<py::tuple, int> moveToIndexMap;
+	static const std::map<int, py::tuple> indexToMoveMap;
 	enum State
 	{
 		EMPTY,
@@ -15,9 +19,14 @@ public:
 		ARROW,
 	};
 	State board[10][10];
+	py::tuple blackAmazons[4];
+	py::tuple whiteAmazons[4];
+	State turn;
+
+	static int moveToIndex(py::tuple move);
+	static py::tuple indexToMove(int index);
 
 	Board();
+	Board(const Board& aBoard);
 };
-
-
 #endif
---
+++ Amazons_CPP/main.cpp
@@ -0,0 +1,19 @@
+#include "Board.h"
+
+PYBIND11_MODULE(Amazons, m, py::mod_gil_not_used())
+{
+	py::class_<Board>(m, "Board")
+		.def(py::init<>())
+		.def(py::init<const Board&>())
+		.def_static("moveToIndex", &Board::moveToIndex)
+		.def_static("indexToMove", &Board::indexToMove)
+		.def_readwrite("board",&Board::board)
+		.def_readwrite("blackAmazons", &Board::blackAmazons)
+		.def_readwrite("whiteAmazons", &Board::whiteAmazons)
+		.def_readwrite("turn", &Board::turn);
+	py::enum_<Board::State>(m, "State")
+		.value("EMPTY", Board::State::EMPTY)
+		.value("WHITE", Board::State::WHITE)
+		.value("BLACK", Board::State::BLACK)
+		.value("ARROW", Board::State::ARROW);
+};
