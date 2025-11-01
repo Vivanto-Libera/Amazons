63c00fc4e4f56850191f729538ffa947643419e1
Vivanto <1425078256@qq.com>
2025-11-01 17:11:34 +0800

    Finished

--- Amazons_CPP/Board.h
+++ Amazons_CPP/Board.h
@@ -3,6 +3,8 @@
 #include <pybind11/pybind11.h>
 #include <map>
 #include <vector>
+#include <string>
+#include <array>
 
 namespace py = pybind11;
 using namespace pybind11::literals;
@@ -10,32 +12,37 @@ using namespace pybind11::literals;
 class Board
 {
 public:
-	static const std::map<py::tuple, int> moveToIndexMap;
-	static const std::map<int, py::tuple> indexToMoveMap;
+	static const std::map<std::string, int> moveToIndexMap;
+	static const std::map<int, std::string> indexToMoveMap;
 	static const int rowDirection[8];
 	static const int colDirection[8];
 
 	enum State
 	{
-		EMPTY,
-		WHITE,
-		BLACK,
-		ARROW,
+		EMPTY=0,
+		WHITE=-1,
+		BLACK=1,
+		ARROW=2,
 	};
 	State board[10][10];
-	py::tuple blackAmazons[4];
-	py::tuple whiteAmazons[4];
+	int blackAmazons[4];
+	int whiteAmazons[4];
 	State turn;
 
-	py::tuple isTerminal();
+	State isTerminal();
 	std::vector<int> legalMoves();
+	void applyMove(int index);
+	py::tuple neuralworkInput();
 
-	static int moveToIndex(py::tuple move);
-	static py::tuple indexToMove(int index);
+	static std::string intToString(int moveIndex[]);
+	static std::array<int,5> stringToInt(std::string str);
+	static int stringToIndex(std::string str);
+	static int locationToIndex(int x, int y);
+	static std::array<int, 2> indextoLocation(int index);
 
 	Board();
 	Board(const Board& aBoard);
 private:
-	std::vector<py::tuple> arrows(py::tuple amazon, int row, int col);
+	std::vector<std::array<int ,2>> arrows(int amazonX, int amazonY, int row, int col);
 };
 #endif
--- Amazons_CPP/main.cpp
+++ Amazons_CPP/main.cpp
@@ -1,21 +1,34 @@
 #include "Board.h"
 
-PYBIND11_MODULE(Amazons, m, py::mod_gil_not_used())
+PYBIND11_MODULE(Amazons, m)
 {
+	py::enum_<Board::State>(m, "State")
+		.value("EMPTY", Board::State::EMPTY)
+		.value("WHITE", Board::State::WHITE)
+		.value("BLACK", Board::State::BLACK)
+		.value("ARROW", Board::State::ARROW)
+		.export_values();
 	py::class_<Board>(m, "Board")
 		.def(py::init<>())
 		.def(py::init<const Board&>())
 		.def("isTerminal", &Board::isTerminal)
 		.def("legalMoves", &Board::legalMoves)
-		.def_static("moveToIndex", &Board::moveToIndex)
-		.def_static("indexToMove", &Board::indexToMove)
-		.def_readwrite("board",&Board::board)
-		.def_readwrite("blackAmazons", &Board::blackAmazons)
-		.def_readwrite("whiteAmazons", &Board::whiteAmazons)
+		.def("neuralworkInput", &Board::neuralworkInput)
+		.def("applyMove", &Board::applyMove)
+		.def("board", [](const Board& b)
+			{
+				py::list ls;
+				for (int i = 0; i < 10; i++)
+				{
+					py::list inner;
+					for (int j = 0; j < 10; j++)
+					{
+						inner.append(b.board[i][j]);
+					}
+					ls.append(inner);
+				}
+				return ls;
+			})
+		.def_static("stringToIndex", &Board::stringToIndex)
 		.def_readwrite("turn", &Board::turn);
-	py::enum_<Board::State>(m, "State")
-		.value("EMPTY", Board::State::EMPTY)
-		.value("WHITE", Board::State::WHITE)
-		.value("BLACK", Board::State::BLACK)
-		.value("ARROW", Board::State::ARROW);
 };
